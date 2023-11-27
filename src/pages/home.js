9ca78c4912d384047e433704f35f49b0a65899d0
Ali Berk Çalışkan <56911685+aliberkcaliskan@users.noreply.github.com>
2023-11-28 01:34:16 +0300

    home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -41,9 +41,9 @@ export default function Home() {
     setChartOptions(options);
   }, []);
   return (
-    <>
+    <div className="bg-gainsboro">
       <Header />
-      <div className="px-44 pt-20 w-full h-full bg-gainsboro relative overflow-hidden">
+      <div className="px-44 pt-20 pb-20 w-full h-full bg-gainsboro relative overflow-scroll">
         <div className="flex justify-center pt-12 rounded-md cursor-pointer">
           <img
             className="p-2 shadow-button bg-white-0"
@@ -70,7 +70,6 @@ export default function Home() {
               CTA Button
             </div>
             <div className="cursor-pointer text-grayx-200 font-semibold">
-              {" "}
               Secondary ➞
             </div>
           </div>
@@ -147,9 +146,8 @@ export default function Home() {
             is executed just once, while the second phase runs continuously.
           </p>
         </div>
-
-        <div className="flex gap-8 w-full pt-12 h-full">
-          <div className="bg-white-200 rounded-md p-5 h-[466px] w-full">
+        <div className="flex w-full pt-12 h-full">
+          <div className="bg-white-200 rounded-md p-5 w-2/6 ">
             <Lottie loop play animationData={lottieJson2} />
             <div className=" ">
               <h6 className="text-xl font-semibold mt-5"> Integration</h6>
@@ -158,18 +156,18 @@ export default function Home() {
               </span>
             </div>
           </div>
-          <div className="bg-white-200 rounded-md p-5 h-[466px] w-full">
+          <div className="bg-white-200 rounded-md p-5 w-2/6 mx-8">
             <Lottie loop play animationData={lottieJson2} />
-            <div className="">
+            <div>
               <h6 className="text-xl font-semibold mt-5"> Integration</h6>
               <span className="text-sm text-darkBlue-200 font-semibold mt-5 block">
                 Learn More ➔
               </span>
             </div>
           </div>
-          <div className="bg-white-200 rounded-md p-5 h-[466px]   w-full">
+          <div className="bg-white-200 rounded-md p-5 w-2/6 ">
             <Lottie loop play animationData={lottieJson2} />
-            <div className="">
+            <div>
               <h6 className="text-xl font-semibold mt-5"> Integration</h6>
               <span className="text-sm text-darkBlue-200 font-semibold mt-5 block">
                 Learn More ➔
@@ -177,28 +175,91 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="w-3/6 h-[18rem] lg:h-[20rem] xl:h-[30rem] bg-white-200 p-4 rounded-md flex flex-col">
-        <div className="bg-white-200 rounded-md p-5 h-[466px]   w-full">
-            <Lottie loop play animationData={lottieJson2} />
-            <div className="">
+        <div className="mt-8 w-full  h-full flex">
+          <div className="bg-white-200 rounded-md p-5 w-[66.15%] justify-around ">
+            <Lottie
+              loop
+              play
+              animationData={lottieJson2}
+              className="h-[22rem]"
+            />
+            <div>
               <h6 className="text-xl font-semibold mt-5"> Integration</h6>
               <span className="text-sm text-darkBlue-200 font-semibold mt-5 block">
                 Learn More ➔
               </span>
             </div>
           </div>
-          <div className="bg-white-200 rounded-md p-5 h-[466px]   w-full">
-            <Lottie loop play animationData={lottieJson2} />
-            <div className="">
+          <div className="w-[2.3rem]"></div>
+          <div className="bg-white-200 rounded-md p-5  w-[32%]">
+            <Lottie
+              loop
+              play
+              animationData={lottieJson2}
+              className="h-[22rem]"
+            />
+            <div>
               <h6 className="text-xl font-semibold mt-5"> Integration</h6>
               <span className="text-sm text-darkBlue-200 font-semibold mt-5 block">
                 Learn More ➔
               </span>
             </div>
           </div>
+        </div>
 
+        <div className="flex justify-center item-center pt-20">
+          <div className="bg-white-0 text-darkBlue-200 py-3 px-5 font-semibold shadow-button rounded-md cursor-pointer">
+            Get in-dept details ->
+          </div>
         </div>
       </div>
-    </>
+
+      <div className="w-screen bg-white-0 justify-center flex items-center py-12">
+        <div className="flex justify-center items-center pt-12 rounded-md cursor-pointer flex-col">
+          <img
+            className=" my-3 p-2 w-9 shadow-button bg-white-0"
+            src="/assets/stars-icon.svg"
+            alt="star-icon"
+          />
+          <h6 className="font-bold text-[40px]"> About Algorithm X</h6>
+          <p className="text-center w-3/6 pt-4 text-grayx-200 font-normal">
+            Algorithm X is a company on a mission of solving the data
+            complexities that all businesses face with the aim of simplifying
+            the landscape of marketing, we are an experienced team with a very
+            ambitious idea and a deep understanding of the challenges that
+            marketers are facing nowadays, our team has harnessed the power of
+            cutting-edge technology to simplify the complex and empower brands
+            to forge profound connections with their customers.
+          </p>
+          <div className="mt-[60px] bg-white-0 text-darkBlue-200 py-3 px-5 font-semibold shadow-button rounded-md cursor-pointer">
+            Get in-dept details ->
+          </div>
+        </div>
+      </div>
+      <div className="px-44 pb-20  pt-20 w-full h-full bg-gainsboro relative flex gap-8">
+        <div className="w-3/6">
+          <img className="w-full" src="/assets/home/card.svg" alt="star-icon" />
+        </div>
+        <div className="w-3/6 flex justify-center flex-col">
+          <img
+            className="p-2 shadow-button w-9 bg-white-0"
+            src="/assets/stars-icon.svg"
+            alt="star-icon"
+          />
+          <h6 className="text-[40px]  mt-4 font-bold"> Our Mission</h6>
+          <p className=" mt-4  text-grayx-200">
+            At Algorithm X, our mission is to simplify and empower marketers in
+            the ever-evolving world of big data. We provide state-of-the-art
+            technology and insights to decode user behavior, predict their
+            actions, and create immersive experiences that resonate deeply with
+            audiences. We focus on simplifying marketing, enabling profound
+            brand attraction at an unprecedented scale.
+          </p>
+          <div className="mt-12  w-56 bg-white-0 text-darkBlue-200 py-3 px-5 font-semibold shadow-button rounded-md cursor-pointer">
+            Get in-dept details ->
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
